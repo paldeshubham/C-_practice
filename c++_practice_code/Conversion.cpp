e88b85cd02bd8d5bd010f47064456854fbf9fe7a
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-12-09 08:06:26 +0000

    conversion done

--- c++_practice_code/Conversion.cpp
+++ c++_practice_code/Conversion.cpp
@@ -1,111 +1,138 @@
 #include<iostream>
 using namespace std;
-
-class stack
-{
+class stack{
     char data[20];
     int top;
-public:
-    stack()
-    {
-        top = -1;
-    }
-    char topdata()
-    {
-        return (data[top]);
-    }
-    void push(char x) {
-        data[++top] = x;
-    }
-    char pop()
-    {
-        return (data[top--]);
-    }
-    int empty()
-    {
-        if (top == -1)
-            return 1;
-        return 0;
-    }
-    int full()
-    {
-        if (top == 19)
+    public:
+        stack()
         {
+            top=-1;
+        }
+        char topdata()
+        {
+            return (data[top]);
+        }
+        void push(char x)
+        {
+            data[++top]=x;
+        }
+        char pop()
+        {
+            return(data[top--]);
+        }
+        int empty()
+        {
+            if(top==-1)
             return 1;
             return 0;
         }
-    }
+        int full()
+        {
+            if(top==19)
+            return 1;
+            return 0;
+        }
 };
 
 int precedence(char x)
 {
-    if (x == '(')
-        return 0;
-
-    if (x == '+' || x == '-')
-        return 1;
-
-    if (x == '*' || x == '/' || x == '%')
-        return 2;
-
+    if(x=='(')
+    return 0;
+    
+    if(x=='+'||x=='-')
+    return 1;
+    
+    if(x=='*'||x=='/'||x=='%')
+    return 2;
+    
     return 3;
+    
 }
-
-void infix_postfix(char infix[20], char postfix[20])
+void infix_postfix(char infix[20] , char postfix[20])
 {
+    int i=0,j=0;
     stack s;
-    int i = 0; // index for infix array
-    int j = 0; // index for postfix array
-    char token, x;
-
-    for (i = 0; infix[i] != '\0'; i++)
+    char token,x;
+    for(i=0;infix[i]!='\0';i++)
     {
-        token = infix[i];
-        if (isalnum(token))
+        token=infix[i];
+        if(isalnum(token))
         {
-            postfix[j] = token;
-            j++;
+            s.push(token);
         }
         else
         {
-            if (token == '(')
+            if(token=='(')
             {
                 s.push(token);
+                
             }
-            else if (token == ')')
+            else if(token==')')
             {
-                while ((x = s.pop()) != '(')
+                while((x=s.pop())!='(')
                 {
-                    postfix[j] = x;
+                    postfix[j]=s.pop();
                     j++;
                 }
             }
             else
             {
-                while (!s.empty() && (precedence(token) <= precedence(s.topdata())))
+                while(!s.empty() && precedence(token) < precedence(s.topdata()))
                 {
-                    postfix[j] = s.pop();
+                    postfix[j]=s.pop();
                     j++;
                 }
                 s.push(token);
             }
         }
     }
-    while (!s.empty())
+    while(!s.empty())
     {
-        postfix[j] = s.pop();
+        postfix[j]=s.pop();
         j++;
     }
-    postfix[j] = '\0';
+    postfix[j]='\0';
 }
-
+void reverse(char a[20], char b[20])
+{
+    int i,j=0;
+    for(i=0;a[i]!='\0';i++)
+    {
+        
+    }
+    i--;
+    for(j=0;i>=0;j++,i--)
+    {
+        if(a[i]=='(')
+        {
+            b[j]=')';
+        }
+        if(a[i]==')')
+        {
+            b[j]='(';
+        }
+        else{
+            b[j]=a[i];
+        }
+    }
+    b[j]='\0';
+    
+}
+void infix_prefix(char infix[20], char prefix[20])
+{
+    char pre[20],in[20];
+    reverse(infix,in);
+    infix_postfix(in,pre);
+    reverse(pre,prefix);
+}
 int main()
 {
-    char infix[20], postfix[20];
-    cout<<"entre infix expression:";
+    char infix[20],postfix[20],prefix[20];
     cin>>infix;
-    
     infix_postfix(infix,postfix);
     cout<<"postfix expression is:";
     cout<<postfix;
+    infix_prefix(infix,prefix);
+    cout<<"\nprefix expression is:";
+    cout<<prefix;
 }
